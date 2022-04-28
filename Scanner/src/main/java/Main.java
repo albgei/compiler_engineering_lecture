abe355fc9f9c9974b79e41ff39089d2bb979b969
albgei <albert@geisbauer.net>
2022-04-28 11:51:05 +0200

    performance testing

--- Scanner/src/main/java/Main.java
+++ Scanner/src/main/java/Main.java
@@ -8,13 +8,25 @@ public class Main {
             """;
 
     public static void main(String[] args) {
+        long start = System.nanoTime();
         LoxScanner scanner = new LoxScanner(program);
-        scanner.scan();
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Zeit " + i + ": " + (System.nanoTime() - start));
+            start = System.nanoTime();
+            scanner.scan();
+        }
+        System.out.println("Zeit: " + (System.nanoTime() - start));
+        start = System.nanoTime();
         LoxScanner scanner1 = new LoxScanner("print \"Hello World\";");
         scanner1.scan();
+        System.out.println("Zeit: " + (System.nanoTime() - start));
+        start = System.nanoTime();
         LoxScanner scanner2 = new LoxScanner("12.45");
         scanner2.scan();
+        System.out.println("Zeit: " + (System.nanoTime() - start));
+        start = System.nanoTime();
         LoxScanner scanner3 = new LoxScanner("//Kommentar ohne Sinn + - * / oops@ kp");
         scanner3.scan();
+        System.out.println("Zeit: " + (System.nanoTime() - start));
     }
 }
